8ad2969ef1770a597436f338f8345ba0100318c3
Fireball19 <63670109+Fireball19@users.noreply.github.com>
2026-01-01 17:24:43 +0100

    Add ui best score

---
+++ Assets/Scripts/UIBestScore.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[DefaultExecutionOrder(1000)]
+public class UIBestScore : MonoBehaviour
+{
+    public TMP_Text bestScoreText;
+
+    void Start()
+    {
+        if (SaveManager.Instance != null) {
+            bestScoreText.text = "Best Score : " + SaveManager.Instance.bestScore + " Name : " + SaveManager.Instance.name;
+        }
+    }
+}
